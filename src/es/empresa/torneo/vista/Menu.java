11570a35c00f082c98773befbfd98693a339face
Casandra Carolina Iglesias Martínez <cciglesiasmartinez@gmail.com>
2025-04-30 03:48:30 +0200

    Se añaden menus básicos de consola
    Añadidos menús básicos modo texto a modo de prueba, invocados desde el
    punto de entrada de la aplicación.

--- src/es/empresa/torneo/vista/Menu.java
+++ src/es/empresa/torneo/vista/Menu.java
@@ -1,5 +1,37 @@
 package es.empresa.torneo.vista;
 
-public class Menu {
+import java.util.Scanner;
 
+public class Menu {
+	
+	public static Scanner entradaTeclado = new Scanner(System.in);
+	
+	public static void mostrarMenu() {
+		System.out.println("Aplicación para la gestión de torneos e-Sports");
+		System.out.println("\n");
+		System.out.println("1. Torneos");
+		System.out.println("2. Equipos");
+		System.out.println("3. Jugadores");
+		System.out.println("4. Resultados");
+		System.out.println("\n");
+		System.out.println("Por favor, escoge una opción");
+		Integer opcion = entradaTeclado.nextInt();
+		procesarMenu(opcion);
+	}
+	
+	public static void procesarMenu(Integer opcion) {
+		switch(opcion) {
+			case(1):
+				mostrarSubmenuTorneos();
+				break;
+			default:
+				System.out.println("Opción no disponible\n");
+				break;
+			}
+	}
+	
+	public static void mostrarSubmenuTorneos() {
+		System.out.println("Submenú de torneos\n");
+		mostrarMenu();
+	}
 }
