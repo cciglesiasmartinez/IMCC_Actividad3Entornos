68957cc0bdbaa3a4037258c728813efed7e88f60
Casandra Carolina Iglesias Martínez <cciglesiasmartinez@gmail.com>
2025-04-30 06:41:27 +0200

    Realiza cambios en la estructuración de los menús

--- src/es/empresa/torneo/vista/Menu.java
+++ src/es/empresa/torneo/vista/Menu.java
@@ -2,36 +2,57 @@ package es.empresa.torneo.vista;
 
 import java.util.Scanner;
 
+import es.empresa.torneo.control.GestionTorneos;
+
 public class Menu {
 	
 	public static Scanner entradaTeclado = new Scanner(System.in);
+	public static GestionTorneos gestionTorneos = GestionTorneos.getInstance();
 	
-	public static void mostrarMenu() {
-		System.out.println("Aplicación para la gestión de torneos e-Sports");
-		System.out.println("\n");
+	public static void procesarMenu() {
+		System.out.println("> Aplicación para la gestión de torneos e-Sports");
+		System.out.println("");
 		System.out.println("1. Torneos");
 		System.out.println("2. Equipos");
 		System.out.println("3. Jugadores");
 		System.out.println("4. Resultados");
-		System.out.println("\n");
-		System.out.println("Por favor, escoge una opción");
+		System.out.println("");
+		System.out.println("Por favor, escoge una opción: ");
 		Integer opcion = entradaTeclado.nextInt();
-		procesarMenu(opcion);
-	}
-	
-	public static void procesarMenu(Integer opcion) {
 		switch(opcion) {
 			case(1):
-				mostrarSubmenuTorneos();
+				procesarSubmenuTorneos();
 				break;
 			default:
 				System.out.println("Opción no disponible\n");
+				procesarMenu();
 				break;
-			}
+		}
 	}
 	
-	public static void mostrarSubmenuTorneos() {
-		System.out.println("Submenú de torneos\n ");
-		mostrarMenu();
+	public static void procesarSubmenuTorneos() {
+		System.out.println("> Submenú de torneos");
+		System.out.println("");
+		System.out.println("1. Listar torneos");
+		System.out.println("");
+		System.out.println("Por favor, escoge una opción: ");
+		Integer opcion = entradaTeclado.nextInt();
+		switch(opcion) {
+			case(1):
+				System.out.println("Lista de torneos activos: ");
+				System.out.println(gestionTorneos.getTorneos().toString() + "\n");
+				break;
+			default:
+				System.out.println("Opción no disponible.");
+				break;
+		}
+		procesarMenu();
+	}
+	
+	public static void procesarSubmenuEquipos() {
+		System.out.println("> Submenú de equipos");
+		System.out.println("");
+		System.out.println("1. Mostrar equipos");
+		System.out.println("2. Registrar");
 	}
 }
